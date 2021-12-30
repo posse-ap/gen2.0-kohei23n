7b1ce8b1cdcd90ad8795d6f36dd3a3ad06b4fea1
kohei23n <kohei.s.n@gmail.com>
2021-12-30 11:24:07 +0900

    week25 まで title修正

--- www/html/index.php
+++ www/html/index.php
@@ -2,6 +2,7 @@
 require('quiz.php');
 
 
+// title表示用
 // big_questions テーブルから '東京の難読地名クイズ'、'広島県の難読地名クイズ'をそれぞれ取得
 // WHERE で一つだけ取得すると、foreach() で書いても大丈夫
 $sql1 = "SELECT * FROM big_questions WHERE id = 1";
@@ -10,6 +11,8 @@ $sql2 = "SELECT * FROM big_questions WHERE id = 2";
 // 上で定義した変数に対し処理
 $stmt1 = $db->query($sql1);
 $stmt2 = $db->query($sql2);
+
+
 
 
 ?>
@@ -21,42 +24,35 @@ $stmt2 = $db->query($sql2);
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>quizy</title>
-</head>
-
-<body>
   <?php
   $url = $_SERVER['REQUEST_URI'];
   if(strstr($url, '1')) : ?>
     <?php foreach ($stmt1 as $row1) : ?>
-    <p>
-      <a href="/quiz?question_id=<?php echo $row1['id']; ?>"><?php echo $row1['id'] . '：' . $row1['name']; ?></a>
-    </p>
+      <title>ガチで東京の人しか解けない！＃<?php echo $row1['name']; ?></title>
     <?php endforeach; ?>
+
   <?php elseif (strstr($url, '2')): ?>
     <?php foreach ($stmt2 as $row2) : ?>
-    <p>
-      <a href="/quiz?question_id=<?php echo $row2['id']; ?>"><?php echo $row2['id'] . '：' . $row2['name']; ?></a>
-    </p>
+      <title>ガチで東京の人しか解けない！＃<?php echo $row2['name']; ?></title>
     <?php endforeach; ?>
+
   <?php endif; ?>
+  <link rel="stylesheet" href="chimei.css">
+</head>
+
+<body>
+  <div id="quizContainer" class="container">
+    <div class="qDiv">
+      <h1 class="nameunderline">hello</h1>
+    </div>
 
 
 
+  </div>
 
 
 
-  
-
-
-  
-
-
-  
-  
-
-
-
+ <script src="chimei.js"></script>
 </body>
 
 </html>
--- www/html/quiz.php
+++ www/html/quiz.php
@@ -8,8 +8,8 @@ try {
 
   $db = new PDO($dsn, $user, $password);
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo '成功'
-;
+  // echo '成功';
+
 } catch (PDOException $e) {
 
   echo '接続失敗' . $e->getMessage();
