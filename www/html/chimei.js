260d95a7f3f0b70490508bfd7504e6566e933064
kohei23n <kohei.s.n@gmail.com>
2022-01-10 09:53:38 +0900

    week26 終了

--- www/html/chimei.js
+++ www/html/chimei.js
@@ -1,29 +1,12 @@
-for (let i = 0; i < 10; i++) {
-
-    //ボタンシャッフル機能
-    function shuffle(arr) {
-        for (let i = arr.length - 1; i > 0; i--) { // i = ランダムに選ぶ終点のインデックス
-          let j = Math.floor(Math.random() * (i + 1));  // j = 範囲内から選ぶランダム変数
-          [arr[j], arr[i]] = [arr[i], arr[j]]; // 分割代入 i と j を入れ替える
-        }
-        return arr;
-    };
-    let shuffledChoices = shuffle(choices[i]);
 
     //ボタンの作成
     let ul = document.createElement('ul');
     ul.innerHTML = `<li class="q" id="c${i}.0" onclick="clickfunction(${i},0)">${choices[i][0]}</li>`
                    + `<li class="q" id="c${i}.1" onclick="clickfunction(${i},1)">${choices[i][1]}</li>`
                    + `<li class="q" id="c${i}.2" onclick="clickfunction(${i},2)">${choices[i][2]}</li>`;
-    ul.classList.add('ul');
-    qDiv.appendChild(ul);
+    
 
-    //回答エリア（正解・不正解表示）作成
-    let ansarea = document.createElement('div');
-    ansarea.id = `ansarea${i}`;
-    qDiv.appendChild(ansarea);
 
-}
 
 //ボタンをクリックした時の処理
 let clickfunction = function (questionnumber, chosennumber) {
@@ -34,31 +17,13 @@ let clickfunction = function (questionnumber, chosennumber) {
     let answer = document.getElementById(`c${questionnumber}.${answernumber}`);
     //選んだ選択肢のindexを取得
     let chosen = document.getElementById(`c${questionnumber}.${chosennumber}`);
-    //回答エリア（正解・不正解表示）のタイトル作成
-    let ansarea = document.getElementById(`ansarea${questionnumber}`);
-    let resultTitle = document.createElement('p');
-    ansarea.appendChild(resultTitle);
-
-    //回答エリア（正解・不正解表示）の説明文作成
-    let resultBody = document.createElement('p');
-    //10個できた ansarea を配列として扱う
-    let ansareaChoice = [ansarea];
-    ansareaChoice.forEach(ansarea => {
-        ansarea.classList.add('ansarea');
-        //回答の説明
-        resultBody.classList.add('ansexp');
-        resultBody.innerHTML = `正解は「${answers[questionnumber]}」です！`;
-        ansarea.appendChild(resultBody);
-    });        
 
     if (chosennumber === answernumber) {
         answer.classList.add('correct');
-        resultTitle.innerHTML = `<p class="c_ansmsg">正解です！</p>`;
     } else {
         //不正解の時は正解の選択肢も表示
         chosen.classList.add('wrong');  
         answer.classList.add('correct'); 
-        resultTitle.innerHTML = `<p class="w_ansmsg">不正解です！</p>`;
     }
 
     //一回クリックしたら再度クリックできなくする
--- www/html/quiz.php
+++ www/html/quiz.php
@@ -7,9 +7,9 @@ require('dbconnect.php');
 $id = $_GET['id'];
 
 // title表示用
-$big_questions_stmt = $db->prepare("SELECT * FROM big_questions WHERE id = ?");
-$big_questions_stmt->execute(array($id));
-$big_questions_results = $big_questions_stmt->fetch();
+$title_stmt = $db->prepare("SELECT * FROM big_questions WHERE id = ?");
+$title_stmt->execute(array($id));
+$title = $title_stmt->fetch();
 
 
 // 画像 & 問題番号表示用
@@ -27,7 +27,7 @@ $questions_results = $questions_stmt->fetchAll();
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title><?= $big_questions_results['name'] ?></title>
+  <title><?= $title['name'] ?></title>
   <link rel="stylesheet" href="chimei.css">
 </head>
 
@@ -38,19 +38,44 @@ $questions_results = $questions_stmt->fetchAll();
     <h1 class="nameunderline"><?= $question_result['id'] ?>.この地名はなんて読む？</h1>
     <img src="img/<?= $question_result['image'] ?>" alt="">
 
-    <ul>
+    <ul class="ul">
       <?php  
       // 選択肢表示用
       $choices_stmt = $db->prepare("SELECT * FROM choices WHERE question_id = ?");
-      // 
       $choices_stmt->execute(array($question_result['id']));
-      $choices_results = $choices_stmt->fetchAll();
-      foreach ($choices_results as $choices_result) :
+      $choices = $choices_stmt->fetchAll();
+      // 正解の選択肢表示用
+      $answer_stmt = $db->prepare("SELECT * FROM choices WHERE question_id = ? AND valid = 1");
+      $answer_stmt->execute(array($question_result['id']));
+      $answer = $answer_stmt->fetch();
+      // 変数名前をわかりやすく
+      $question_number = $question_result['id'];
       ?>
 
-      <li class="q"><?= $choices_result['name'] ?></li>
+      <?php 
+      // foreach に埋め込む前に配列をシャッフル
+      shuffle($choices); 
+      foreach ($choices as $choice) :
+      ?>
+      <li class="q" 
+          id="choice<?= $option_number; ?>_<?= $question_result['id']; ?>" 
+          onclick="clickfunction(<?= $question_result['id']; ?>, <?= $question_result['id']; ?>)">
+        <?= $choice['name'] ?>
+      </li>
+
+      <!-- id="c${i}.0" onclick="clickfunction(${i},0)" -->
       <?php endforeach; ?>
     </ul>
+
+    <div class="ansarea">
+      <p class="c_ansmsg">正解！</p>
+      <p class="ansexp">正解は「<?= $answer['name'] ?>」です！</p>
+    </div>
+
+    <div class="ansarea">
+      <p class="w_ansmsg">不正解！</p>
+      <p class="ansexp">正解は「<?= $answer['name'] ?>」です！</p>
+    </div>
 
 
 
