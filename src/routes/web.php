58d2196e96bf139292b44c35677549651fa29a4d
kohei23n <kohei.s.n@gmail.com>
2022-06-08 03:07:46 +0900

    week33 done

---
+++ src/routes/web.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route::get('hello/{msg?}', function ($msg='no message.') {
+//     $html = <<<EOF
+//     <html>
+//     <head>
+//     <title>Hello</title>
+//     <style>
+//     body {font-size:16pt; color:#999;}
+//     h1 {font-size:100pt; text-align:right; color: #eee;
+//     margin: -40px 0 -50px 0; }
+//     </style>
+//     </head>
+//     <body>
+//         <h1>Hello</h1>
+//         <p>{$msg}</p>
+//         <p>これはサンプルで作ったページです。</p>
+//     </body>
+//     </html>
+//     EOF;
+
+//     return $html;
+// });
+
+// Route::get('hello', 'HelloController');
+Route::get('hello', 'HelloController@index');
+// Route::get('hello/other', 'HelloController@other');
+
+
+// Route::get('hello', function () {
+//     return '<html><body><h1>Hello</h1><p>This is sample page.</p></body></html>';
+// });
+
+
+
+
