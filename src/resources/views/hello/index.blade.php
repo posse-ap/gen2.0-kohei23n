6c075aecdacfc9816faf3993b4cce24f871d8fd1
kohei23n <kohei.s.n@gmail.com>
2022-06-10 19:33:36 +0900

    done

--- src/resources/views/hello/index.blade.php
+++ src/resources/views/hello/index.blade.php
@@ -1,15 +1,18 @@
-<html>
-<head>
-  <title>Hello/Index</title>
-  <style></style>
-</head>
-<body>
-  <h1>Blade/Index</h1>
-  <p>&#064;foreachディレクティブの例</p>
-  <ol>
-  @foreach ($data as $item)
-  <li>{{$item}}
-  @endforeach
-  </ol>
-</body>
-</html>
+@extends('layouts.helloapp')
+
+@section('title', 'Index')
+
+@section('menubar')
+  @parent
+  インデックスページ
+@endsection
+
+@section('content')
+  <p>ここが本文のコンテンツです。</p>
+  <p>Controller value<br>'message' = {{$message}}</p>
+  <p>ViewComposer value<br>'view_message' = {{$view_message}}</p>
+@endsection
+
+@section('footer')
+copyright 2020 tuyano.
+@endsection
