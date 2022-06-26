b70736ff51d749a0118664a3f96f9533690781c8
kohei23n <kohei.s.n@gmail.com>
2022-06-26 19:50:49 +0900

    修正

--- src/resources/views/admin/choices/list.blade.php
+++ src/resources/views/admin/choices/list.blade.php
@@ -26,7 +26,7 @@
         </ul>
         <div>
           <a href="/admin/choices/{{ $question->id }}/add">＋新しい選択肢を追加</a>
-          <a href="/admin/small_questions/{{ $question->id }}">設問の一覧に戻る</a>
+          <a href="/admin/small_questions/{{ $question->big_question_id }}">設問の一覧に戻る</a>
         </div>
       @endsection 
 
