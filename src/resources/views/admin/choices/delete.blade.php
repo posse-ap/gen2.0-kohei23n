a03b5facc4a033513db453497f9b1efd6d94fe10
kohei23n <kohei.s.n@gmail.com>
2022-06-29 16:06:03 +0900

    week41,44：@extendsの使い方が間違っていたため全てのdelete.blade.phpを修正

--- src/resources/views/admin/choices/delete.blade.php
+++ src/resources/views/admin/choices/delete.blade.php
@@ -1,34 +1,16 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>クイズ一覧</title>
-        <style>
-          div {text-align: center}
-          a {text-align: center}
-        </style>
-    </head>
-    <body>
-      @extends('layouts.app')
+@extends('layouts.app')
 
-      @section('content')
-        <form action="" method="post">
-        <table>
-          @csrf 
-          <input type="hidden" name="id" value="{{ $choice->id }}">
-          <tr>
-            <th>選択肢：</th>
-            <td>{{ $choice->name }}</td>
-          </tr>
-          <tr>
-            <th></th>
-            <td><input type="submit" value="削除"></td>
-          </tr>
-        </table>
-        </form>
-      @endsection 
+@section('title', '選択肢の削除')
 
+@section('content')
+  <form class="edit" action="" method="post">
+    @csrf 
+    <input type="hidden" name="id" value="{{ $choice->id }}">
+    <p>選択肢：</p>
+    <ul class="ul_edit">
+      <li class="q_admin">{{ $choice->name }}</li>
+    </ul>
+    <input type="submit" value="削除">
+  </form>
+@endsection 
 
-    </body>
-</html>
--- src/resources/views/admin/small_questions/delete.blade.php
+++ src/resources/views/admin/small_questions/delete.blade.php
@@ -1,34 +1,13 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>クイズ一覧</title>
-        <style>
-          div {text-align: center}
-          a {text-align: center}
-        </style>
-    </head>
-    <body>
-      @extends('layouts.app')
+@extends('layouts.app')
 
-      @section('content')
-        <form action="" method="post">
-        <table>
-          @csrf 
-          <input type="hidden" name="id" value="{{ $question->id }}">
-          <tr>
-            <th>問題：</th>
-            <img src="/images/{{ $question->image }}" alt="">
-          </tr>
-          <tr>
-            <th></th>
-            <td><input type="submit" value="削除"></td>
-          </tr>
-        </table>
-        </form>
-      @endsection 
+@section('title', '設問の削除')
 
-
-    </body>
-</html>
+@section('content')
+  <form class="edit" action="" method="post">
+    @csrf 
+    <input type="hidden" name="id" value="{{ $question->id }}">
+    <p>問題：</p>
+    <img src="/images/{{ $question->image }}" alt=""><br>
+    <input type="submit" value="削除">
+  </form>
+@endsection 
