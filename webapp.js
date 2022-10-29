8486a0331420bb458c6face097f0d83337854aa4
kohei23n <kohei.s.n@gmail.com>
2022-10-29 19:43:22 +0900

    fix：いらないコメント削除

--- webapp.js
+++ webapp.js
@@ -111,13 +111,6 @@ kiroku.addEventListener('click', function() {
   hidecontent.style.display = "none";
 });
 
-// setTimeout(function() { 
-//   let loadermobile = document.getElementById('loader_mobile');
-//   loadermobile.style.display = "none";
-//   let complete = document.getElementById('complete_mobile');
-//   complete.style.display = "block";
-//  }, 5000);
-
 $("#mobile_modalbtn2").click(function() {
   setTimeout(function() {
     $("#complete_mobile").css("display", "block");
@@ -175,94 +168,6 @@ let twittercircle = document.getElementById('twitter_circle');
 twittercircle.addEventListener('click', function() {
   twittercircle.classList.toggle('twitter_circle_checked');
 })
-
-
-// COLUMN CHART
-
-// google.charts.load("current", {packages:["corechart"]});
-//       google.charts.setOnLoadCallback(drawColumnChart);
-//       function drawColumnChart() {
-
-//         var data = new google.visualization.DataTable();
-//         data.addColumn('number', 'Day of Month');
-//         data.addColumn('number', 'Hours Studied');
-
-//         data.addRows([
-//           [1, 3], 
-//           [2, 4],
-//           [3, 5],
-//           [4, 3],
-//           [5, 0],
-//           [6, 0],
-//           [7, 4],
-//           [8, 2],
-//           [9, 2],
-//           [10, 8],
-//           [11, 8],
-//           [12, 2],
-//           [13, 2],
-//           [14, 1],
-//           [15, 7],
-//           [16, 4],
-//           [17, 4],
-//           [18, 3],
-//           [19, 3],
-//           [20, 3],
-//           [21, 2],
-//           [22, 2],
-//           [23, 6],
-//           [24, 2],
-//           [25, 2],
-//           [26, 1],
-//           [27, 1],
-//           [28, 1],
-//           [29, 7],
-//           [30, 8]
-//         ]);
-
-
-//       var options = {
-//         // width: '85%',
-//         // height: '90%',
-//         chartArea: { width: '85%', height: '85%'},
-//         legend: { position: 'none' },
-//         hAxis: { 
-//           ticks: [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30],
-//           // ticks: {
-//           //   min: 1,
-//           //   max: 30,
-//           //   stepSize: 2,
-//           //   // maxTicksLimit: 16,
-//           // },
-//           gridlines: {color: 'transparent'},
-//           textStyle: {
-//             color: '#b8cddf'
-//           }
-//         },
-//         vAxis: {
-//           gridlines: {color: 'transparent'},
-//           baselineColor: 'transparent',
-//           format: '#h',
-//           textStyle: {
-//             color: '#b8cddf'
-//           }
-//         },
-//         colors:['#36c0f5'],
-//         maintainAspectRatio: false
-
-//       };
-
-//       var chart = new google.visualization.ColumnChart(
-//         document.getElementById('column'));
-
-//       drawChart();
-//       window.addEventListener('resize', drawChart, false);
-//       function drawChart() {
-//         chart.draw(data, options);
-//       }
-//       }
-
-
 
 const column = document.getElementById('column').getContext('2d');
 
@@ -334,53 +239,6 @@ const myColumnChart = new Chart(column, {
     
 });
 
-
-
-
-
-// DONUT CHART 
-
-// STUDY LANGUAGE
-// google.charts.load("current", {packages:["corechart"]});
-//       google.charts.setOnLoadCallback(drawDonut1);
-//       function drawDonut1() {
-//         var data = google.visualization.arrayToDataTable([
-//           ['Task', 'Hours'],
-//           ['HTML',     30],
-//           ['CSS',      20],
-//           ['SQL',  20],
-//           ['SHELL',  20],
-//           ['Javascript',  10],
-//           ['その他',  10],
-//           ['PHP',  5],
-//           ['Laravel',  5],
-//         ]);
-
-//         var options = {
-//           chartArea: {  width: "85%", height: "100%" },
-//           pieHole: 0.4,
-//           slices: {
-//             0: { color: '#0445ec' },
-//             1: { color: '#0f70bd' },
-//             2: { color: '#20bdde' },
-//             3: { color: '#3ccefe' },
-//             4: { color: '#b29ef3' },
-//             5: { color: '#6c46eb' },
-//             6: { color: '#4a17ef' },
-//             7: { color: '#3005c0' },
-//           },
-//           legend: {
-//             position: 'none',
-//           },
-//           pieSliceBorderColor: 'none',
-
-
-//         };
-
-//         var chart = new google.visualization.PieChart(document.getElementById('donut_lang'));
-//         chart.draw(data, options);
-//       }
-
 const donut1 = document.getElementById('donut_lang').getContext('2d');
 
 const myFirstDonutChart = new Chart(donut1, {
@@ -419,36 +277,6 @@ const myFirstDonutChart = new Chart(donut1, {
     },
     
 });
-
-// STUDY CONTENT
-// google.charts.load("current", {packages:["corechart"]});
-//       google.charts.setOnLoadCallback(drawDonut2);
-//       function drawDonut2() {
-//         var data = google.visualization.arrayToDataTable([
-//           ['Task', 'Hours'],
-//           ['N予備校',     40],
-//           ['課題',  40],
-//           ['ドットインストール',      20]
-//         ]);
-
-//         var options = {
-//           chartArea: {  width: "85%", height: "100%" },
-//           pieHole: 0.4,
-//           slices: {
-//             0: { color: '#0445ec' },
-//             1: { color: '#0f70bd' },
-//             2: { color: '#20bdde' }
-//           },
-//           legend: {
-//             position: 'none',
-//           },
-//           pieSliceBorderColor: 'none',
-
-//         };
-
-//         var chart = new google.visualization.PieChart(document.getElementById('donut_content'));
-//         chart.draw(data, options);
-//       }
 
 const donut2 = document.getElementById('donut_content').getContext('2d');
 
