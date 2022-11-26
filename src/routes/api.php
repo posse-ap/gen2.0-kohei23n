7a1474430e0c9f1b44db3e6f19bf18008bc1e010
kohei23n <kohei.s.n@gmail.com>
2022-11-26 20:18:57 +0900

    コンフリクト解消

--- src/routes/api.php
+++ src/routes/api.php
@@ -16,9 +16,6 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-<<<<<<< HEAD
-=======
 
 //20:04 竜大 ここでルーティングつくって、コントローラーはviewじゃなくてjsonとか値をreturnするような設計にすると、
 // 20:04 竜大 ただデータを渡すだけのルーティングができるわけよ
->>>>>>> 83d4ba49c7e907fb23706b37793d00372a8a3725
